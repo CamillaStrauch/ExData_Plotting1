c6b7bf39b18d0d593e7b08b945cbb537b0f21feb
Camilla <helle.strauch@gmail.com>
2014-08-10 23:45:08 +0200

    Adding part2

---
+++ CourseAssignment1/plot2.R
@@ -0,0 +1,31 @@
+# Change to English
+  Sys.setlocale("LC_ALL", "English")
+
+# Load dataset into R
+  filename <- "household_power_consumption.txt"
+
+  df <- read.table(filename,
+                   header=TRUE,
+                   sep=";",
+                   colClasses=c("character", "character", rep("numeric",7)),
+                   na="?")
+
+# convert date and time variables to Date/Time class
+  df$Time <- strptime(paste(df$Date, df$Time), "%d/%m/%Y %H:%M:%S")
+  df$Date <- as.Date(df$Date, "%d/%m/%Y")
+
+# Subset to only use data from date 2007-02-01 and 2007-02-02
+  dates <- as.Date(c("2007-02-01", "2007-02-02"), "%Y-%m-%d")
+  df <- subset(df, Date %in% dates)
+
+# Open an png graphics-device
+  png("plot2.png", width=480, height=480)
+
+# Write to the graphics-device
+  plot(df$Time, df$Global_active_power,
+         type="l",
+         xlab="",
+         ylab="Global Active Power (kilowatts)")
+
+# Close the graphics-device
+  dev.off()
